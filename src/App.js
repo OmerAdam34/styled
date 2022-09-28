b04860c9a91bd4bedc6908efe9d665836d81c063
Ömer Algin <oemer-algin@hotmail.de>
2022-09-28 15:28:52 +0200

    normal css steps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <h2>Styled components</h2>;
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1 className="title">styled components</h1>
+      <button className="btn">click me</button>
+      <div style={{ padding: "2rem" }}>
+        <h2 className="section-title">hello world</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App;
